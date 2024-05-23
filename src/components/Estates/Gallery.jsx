b30de178e97bfd0ec2a2f8253a95a9afa389f866
Judah Oyedele <81442563+Judah1604@users.noreply.github.com>
2024-05-23 10:16:35 +0100

    finished gallery page

---
+++ src/components/Estates/Gallery.jsx
@@ -0,0 +1,31 @@
+import ImgUnderlay from "../../utils/ImgUnderlay";
+
+function Gallery() {
+  return (
+    <>
+      <div className="contaner gallery">
+        <ImgUnderlay title={"Gallery"} />
+        <div className="images mt-5 gap-2 row">
+          <div className="col-md-4"><img src="/gallery/land.JPG" alt="LAND" /></div>
+          <div className="col-md-4"><img src="/gallery/land2.JPG" alt="LAND" /></div>
+          <div className="col-md-4"><img src="/gallery/land3.JPG" alt="LAND" /></div>
+          <div className="col-md-4"><img src="/gallery/land4.JPG" alt="LAND" /></div>
+          <div className="col-md-4"><img src="/gallery/land5.JPG" alt="LAND" /></div>
+          <div className="col-md-4"><img src="/gallery/land6.JPG" alt="LAND" /></div>
+          <div className="col-md-4"><img src="/gallery/land7.JPG" alt="LAND" /></div>
+          <div className="col-md-4"><img src="/gallery/land8.JPG" alt="LAND" /></div>
+          <div className="col-md-4"><img src="/gallery/land9.JPG" alt="LAND" /></div>
+          <div className="col-md-4"><img src="/gallery/land10.JPG" alt="LAND" /></div>
+          <div className="col-md-4"><img src="/gallery/land11.JPG" alt="LAND" /></div>
+          <div className="col-md-4"><img src="/gallery/land12.JPG" alt="LAND" /></div>
+          <div className="col-md-4"><img src="/gallery/land13.JPG" alt="LAND" /></div>
+          <div className="col-md-4"><img src="/gallery/land14.JPG" alt="LAND" /></div>
+          <div className="col-md-4"><img src="/gallery/land15.JPG" alt="LAND" /></div>
+          <div className="col-md-4"><img src="/gallery/land16.JPG" alt="LAND" /></div>
+          <div className="col-md-4"><img src="/gallery/land17.JPG" alt="LAND" /></div>
+        </div>
+      </div>
+    </>
+  );
+}
+export default App;
--- src/components/static/Nav.jsx
+++ src/components/static/Nav.jsx
@@ -49,6 +49,46 @@ function Nav({pathname}) {
 					</div>
 				</div>
 			</div>
+			<div className="nav-item dropdown">
+				<a
+					href="#"
+					className={
+						pathname.includes("/estates")
+							? "nav-link active"
+							: "nav-link"
+					}
+					data-bs-toggle="dropdown"
+				>
+					Our Estates
+					<i class="fa-solid fa-chevron-down"></i>
+				</a>
+				<div className="dropdown-menu dropdown-menu-dark">
+					<div>
+						<a
+							href="/estates"
+							className={
+								pathname === "/estates"
+									? "nav-link active"
+									: "nav-link"
+							}
+						>
+							Estates
+						</a>
+					</div>
+					<div>
+						<a
+							className={
+								pathname === "/about/gallery"
+									? "nav-link active"
+									: "nav-link"
+							}
+							href="/about/gallery"
+						>
+							Gallery
+						</a>
+					</div>
+				</div>
+			</div>
 			<a
 				href="/services"
 				className={
@@ -56,14 +96,6 @@ function Nav({pathname}) {
 				}
 			>
 				Our Services
-			</a>
-			<a
-				href="/estates"
-				className={
-					pathname === "/estates" ? "nav-link active" : "nav-link"
-				}
-			>
-				Our Estates
 			</a>
 			<a
 				href="/contact"
