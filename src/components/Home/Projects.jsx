3800fa3f373a53f5cc79d8a237e3a7478f317e29
Judah Oyedele <81442563+Judah1604@users.noreply.github.com>
2024-06-03 20:02:08 +0100

    Update Projects.jsx

--- src/components/Home/Projects.jsx
+++ src/components/Home/Projects.jsx
@@ -11,6 +11,17 @@ function Projects() {
 					width="480"
 					height="360"
 					className="col-md-6"
+					src="https://youtu.be/0pooo9Bksg0?si=-1GWimlNqzBTKLCa"
+					title="Oakwood Gardens Phase II"
+					frameborder="0"
+					allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+					referrerpolicy="strict-origin-when-cross-origin"
+					allowfullscreen
+				></iframe>
+				<iframe
+					width="480"
+					height="360"
+					className="col-md-6"
 					src="https://www.youtube.com/embed/R62P2m6XLBo"
 					title="Oakwood Gardens Phase II"
 					frameborder="0"
